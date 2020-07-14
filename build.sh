45ca87b42d2d9fbe3a2f6077b6f8fc2d3b411fb1
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-07-14 22:17:12 +0900

    update

---
+++ build.sh
@@ -0,0 +1,8 @@
+# cargo atcoder
+cargo atcoder new abc161
+cargo atcoder test a
+
+cargo run --bin a
+
+# ジャッジビルドコマンド
+cargo build --release --offline --quiet
--- template/AtCoder.rs
+++ template/AtCoder.rs
@@ -1,14 +1,15 @@
+use proconio::{input, fastout};
+
+#[allow(unused_imports)]
+use std::collections::*;
 #[allow(unused_imports)]
 use std::cmp::*;
-use proconio::input;
 
-
+#[fastout]
 fn main() {
-    // n
-    // v_1 v_2 ... v_n
     input!{
-        n: usize,
-        v: [i32; n],
+        // n: usize,
+        // v: [i32; n],
     }
 
     let ans = 42;
