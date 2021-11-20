85c84fd8564b1d0ca6a0b8c3e5a98654c38118c0
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2021-11-21 00:23:02 +0900

    update

--- Marathon/2-opt-HillClimbing.rs
+++ Marathon/2-opt-HillClimbing.rs
@@ -1,18 +1,16 @@
-// 未テスト
-
 // 2-opt 山登り
 
 use std::time::SystemTime;
 
+#[allow(dead_code)]
 struct Yamanobori {
-    path: Vec<usize>, // ノード番号が入る
+    path: Vec<usize>, // ノード番号が入る。ノード番号は、tableのindexとも対応する。
     score: usize,
     table: Vec<Vec<usize>>, // [aノード番号][bノード番号] := a-b 間の距離
-    main_start_time: SystemTime,
 }
-
+#[allow(dead_code)]
 impl Yamanobori {
-    fn new(start_path: Vec<usize>, table: Vec<Vec<usize>>, main_start_time: SystemTime) -> Yamanobori {
+    fn new(start_path: Vec<usize>, table: Vec<Vec<usize>>) -> Yamanobori {
         let mut score = 0;
         let path_length = start_path.len();
 
@@ -25,7 +23,6 @@ impl Yamanobori {
             path: start_path,
             score,
             table,
-            main_start_time,
         }
     }
 
@@ -42,15 +39,18 @@ impl Yamanobori {
     }
 
     // end_time: main関数の開始後からの時間を指す
-    // 始点終点は固定のはず
+    // 始点終点は固定される
     fn run(
         &mut self,
-        end_time: u128, // ミリ秒表記
+        during_time: u128, // 焼きなまし実行時間(ミリ秒)
     ) {
+        let system_time = SystemTime::now();
+        let start_time = system_time.elapsed().unwrap().as_millis();
+
         let mut rand = rand_pcg::Pcg64Mcg::new(890482);
         let path_length = self.path.len();
 
-        while self.main_start_time.elapsed().unwrap().as_millis() < end_time {
+        while system_time.elapsed().unwrap().as_millis() - start_time < during_time {
             for _ in 0..1000 {
                 let mut lci = rand.gen_range(0, path_length); // left cut i
                 let mut rci = rand.gen_range(0, path_length); // right cut i
--- Math/RepeatedSquaring.rs
+++ Math/RepeatedSquaring.rs
@@ -3,7 +3,7 @@ fn repeated_squaring(x: usize, n: usize, p: usize) -> usize {
     if n == 0 {
         1
     } else if n % 2 == 0 {
-        repeated_squaring(x * x % p, n / 2, p)
+        repeated_squaring((x * x) % p, n / 2, p)
     } else {
         (x * repeated_squaring(x, n - 1, p)) % p
     }
